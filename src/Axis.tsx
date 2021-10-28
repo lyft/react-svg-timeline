68199b97e5582cb2ce67d340f57293f5ecfda368
Scarlett Perry <sperry@lyft.com>
2021-10-28 19:54:24 -0400

    fix lineaxis theme

--- src/Axis.tsx
+++ src/Axis.tsx
@@ -1,15 +1,17 @@
 import * as React from 'react'
 import makeStyles from '@material-ui/core/styles/makeStyles'
 import { LineAxisTheme } from './theme/model'
+import { useTimelineTheme } from './theme'
 
-const useAxisStyles = makeStyles({
+const useAxisStyles = makeStyles(() => ({
   axis: (lineAxisTheme: LineAxisTheme) => ({
     stroke: lineAxisTheme.stroke,
     strokeWidth: lineAxisTheme.strokeWidth
   }),
-})
+}))
 
 export const Axis = ({ y }: { y: number }) => {
-  const classes = useAxisStyles()
+  const lineAxisTheme = useTimelineTheme().lineAxis
+  const classes = useAxisStyles(lineAxisTheme)
   return <line x1={0} y1={y} x2="100%" y2={y} className={classes.axis} />
 }
