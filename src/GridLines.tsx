ab25541e6b4c35475b9d132b429a627b01401359
SHAWNA MONERO <smonero@lyft.com>
2021-11-04 12:40:56 -0700

    test

--- src/GridLines.tsx
+++ src/GridLines.tsx
@@ -25,6 +25,7 @@ const gridLineStyle = (theme: Theme) => ({
 })
 
 export const GridLines = ({ height, domain, smallerZoomScale, timeScale, weekStripes }: Props) => {
+  console.log(smallerZoomScale);
   switch (smallerZoomScale) {
     case ZoomLevels.TEN_YEARS:
       return <YearView height={height} domain={domain} timeScale={timeScale} showDecadesOnly={true} />
