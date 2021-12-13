4071ed6eb538909df4bb4dcdfc33da6644daf86e
SHAWNA MONERO <smonero@lyft.com>
2021-12-13 09:56:52 -0800

    fix halves

--- src/layers/GridLines.tsx
+++ src/layers/GridLines.tsx
@@ -35,7 +35,7 @@ export const GridLines = ({ height, domain, smallerZoomScale, timeScale, weekStr
     case ZoomLevels.ONE_WEEK:
       return <HoursView height={height} domain={domain} timeScale={timeScale} doubles={true} ones={false} halves={false} quarters={false} eights={false} />
     case ZoomLevels.ONE_DAY:
-      return <HoursView height={height} domain={domain} timeScale={timeScale} doubles={true} ones={true} halves={true} quarters={false} eights={false} />
+      return <HoursView height={height} domain={domain} timeScale={timeScale} doubles={true} ones={true} halves={false} quarters={false} eights={false} />
     case ZoomLevels.TWELVE_HOURS:
       return <HoursView height={height} domain={domain} timeScale={timeScale} doubles={true} ones={true} halves={true} quarters={true} eights={true} />
     case ZoomLevels.SIX_HOURS:
